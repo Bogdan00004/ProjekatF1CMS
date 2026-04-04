d3d9b94af4fa3d6814a18ef5f25654c963764df8
Bogdan00004 <bogdankralj004@gmail.com>
2026-04-04 14:50:59 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ProjekatF1CMS
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjekatF1CMS.Models
+{
+    [Serializable]
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public UserRole Role { get; set; }
+        public User() { }
+        public User(string username, string password, UserRole role)
+        {
+            Username = username;
+            Password = password;
+            Role = role;
+        }
+    }
+}
